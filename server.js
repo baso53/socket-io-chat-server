a16747a4ff74b6ce4c77b9e747322b4fdcf84339
Sebastijan <baso53@outlook.com>
2018-03-24 17:26:47 +0100

    Initial commit

--- server.js
+++ server.js
@@ -3,27 +3,24 @@
 var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+app.set("port", process.env.PORT || 3000);
 
-app.set('port', process.env.PORT || 3000);
 
 io.on("connection", (socket) => {
     console.log("a user connected");
 
-    io.emit("welcome-message", "WELCOME TO A SOCKET.IO SERVER");
-
     socket.on("disconnect", () => {
         console.log("user disconnected");
     });
-
-    socket.on("chat-message", (msg) => {
-        console.log("Message: " + msg);
-    });
-
 });
 
+
 app.get("/", (req, res) => res.send("You have reached a Socket.io server"));
+app.get("/:msg", (req, res) => {
+    io.emit("message", req.params.msg);
+});
 app.get("/*", (req, res) => res.redirect("/"));
 
-http.listen(app.get('port'), () => {
-  console.log("Express server listening on port " + app.get('port'));
+http.listen(app.get("port"), () => {
+  console.log("Express server listening on port " + app.get("port"));
 });
